66937d0effc39551843d34f0d57c0cebac0c3016
Peggyta <peggyta9096@gmail.com>
2022-11-09 14:21:31 +0330

    build html structure of the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,9 +12,45 @@ import logo from '../images/logo-white.svg';
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+       <header>
+            <div className='logo-container'>
+                <img src={logo} alt='logo-image' />
+            </div>
+            <div className='list-container'>
+                <ul className='navbar-wrapper'>
+                    <li>
+                        <NavLink to="">
+                            محصولات
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="">
+                            تعرفه‌ها
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="">
+                            توسعه‌دهنده‌ها
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="">
+                            تماس با ما
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="">
+                            بیش‌تر
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="">
+                            زرین‌پال‌من
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+       </header>
     );
 };
 
