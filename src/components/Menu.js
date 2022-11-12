2a6b5c3e2a505f48343733c3c5efb8e46a2a52cd
Peggyta <peggyta9096@gmail.com>
2022-11-12 17:13:17 +0330

    correcting html arrangment of hamburger menu items

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,49 +1,51 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-//Image & Icons
-import { FiAlignJustify } from 'react-icons/fi';
-import { FiX } from 'react-icons/fi';
 //Style
 import '../scss/Menu.css';
+import { FiX } from 'react-icons/fi';
+import { FiAlignJustify } from 'react-icons/fi';
 
-const Menu = () => {
+const Menu = ({isClicked, setIsClicked}) => {
+    const clickHandler = () => {
+        setIsClicked(!isClicked)
+    };
     return (
        <header className='hambur-menu'>
-            <nav className='hambur-nav'>
-                <ul className='hambur-wrapper'>
-                    <li>
-                        <NavLink className='hambur-list' to="/price">
-                            تعرفه‌ها
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink className='hambur-list' to="/">
-                           اپلیکیشن
-                        </NavLink>
-                    </li>
-                    <div></div>
-                    <li>
-                        <NavLink className='hambur-list' to="/">
-                            سوالات متداول
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink className='hambur-list' to="/products">
-                            محصولات
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink className='hambur-list' to="/contact-us">
-                            تماس با ما
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink className='hambur-list' to="/my-zarin-pal">
-                            زرین‌پال‌من
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
+            {!isClicked ? 
+                (<ul className='hambur-wrapper'>
+                <li>
+                    <NavLink className='hambur-list' to="/price">
+                        تعرفه‌ها
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink className='hambur-list' to="/">
+                       اپلیکیشن
+                    </NavLink>
+                </li>
+                <div></div>
+                <li>
+                    <NavLink className='hambur-list' to="/">
+                        سوالات متداول
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink className='hambur-list' to="/products">
+                        محصولات
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink className='hambur-list' to="/contact-us">
+                        تماس با ما
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink className='hambur-list' to="/my-zarin-pal">
+                        زرین‌پال‌من
+                    </NavLink>
+                </li>
+            </ul>) : 
+                (<FiX onClick={clickHandler} className='nav-icon' /> )}
        </header>
     );
 };
