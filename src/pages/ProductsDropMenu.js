846097aa002c5877d86cc8abf2b6bd23f18a2a1d
Peggyta <peggyta9096@gmail.com>
2022-11-15 13:48:17 +0330

    add some texts to the links of second dropDown menu

--- src/pages/ProductsDropMenu.js
+++ src/pages/ProductsDropMenu.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import useDropdownMenu from 'react-accessible-dropdown-menu-hook';
 import { FiChevronDown } from 'react-icons/fi';
+import '../scss/Navbar.css';
 
 const ProductsDropMenu = () => {
     const { isOpen, itemProps, buttonProps } = useDropdownMenu(5);
@@ -10,14 +11,30 @@ const ProductsDropMenu = () => {
                 محصولات
                 <FiChevronDown className='drop-down-sign' />
             </button>
-            <div className={ isOpen ? 'products-menu': ''}>
+            <div className={ isOpen ? 'visible': ''} role='menu'>
                 <div>
-                    <a></a>
-                    <a></a>
-                    <a></a>
+                    <a href='#'{...itemProps[0]}>
+                        <p>درگاه پرداخت</p>
+                        <p>مهندسی شده برای فروش بیشتر</p>
+                    </a>
+                    <a href='#'{...itemProps[1]}>
+                        <p>درگاه پرداخت</p>
+                        <p>مهندسی شده برای فروش بیشتر</p>
+                    </a>
+                    <a href='#'{...itemProps[2]}>
+                        <p>درگاه پرداخت</p>
+                        <p>مهندسی شده برای فروش بیشتر</p>
+                    </a>
                 </div>
                 <div>
-
+                    <a href='#'{...itemProps[3]}>
+                        <p>درگاه پرداخت</p>
+                        <p>مهندسی شده برای فروش بیشتر</p>
+                    </a>
+                    <a href='#'{...itemProps[4]}>
+                        <p>درگاه پرداخت</p>
+                        <p>مهندسی شده برای فروش بیشتر</p>
+                    </a>
                 </div>
             </div>
         </div>
