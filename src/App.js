39f6c4cbb579dc4322add9e98edbedce969a5995
Peggyta <peggyta9096@gmail.com>
2022-11-16 15:27:53 +0330

    replace the banner component as the main route

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import './App.css';
+import React,{useState} from 'react';
 import {Route, Routes} from 'react-router-dom';
 //Components
 import Navbar from './components/Navbar';
+import Banner from './components/Banner';
+import Menu from './components/Menu';
 import ContactUs from './pages/ContactUs';
 import Developers from './pages/Developers';
-import More from './pages/More';
 import MyZarinPal from './pages/MyZarinPal';
 import Price from './pages/Price';
 import Products from './pages/Products';
-import Home from './pages/Home';
 
 function App() {
+  const[isClicked, setIsClicked] = useState(false);
   return (
-    <div>
-        <Navbar />
+    <div className='App'>
+        <Navbar isClicked={isClicked} setIsClicked={setIsClicked} />
+        {isClicked ? <Menu /> : null}
       <Routes>
           <Route path="products" element={<Products />} />
           <Route path="price" element={<Price />} />
           <Route path="developers" element={<Developers />} />
           <Route path="contact-us" element={<ContactUs />} />
-          <Route path="more" element={<More />} />
           <Route path="my-zarin-pal" element={<MyZarinPal />} />
-          <Route path="/" exact={true} element={<Home />} />
+          <Route path="/" exact={true} element={<Banner />} />
       </Routes>
     </div>
   );
