5215c08d5adb5ef7cffc20076d50e0334e743fbe
Peggyta <peggyta9096@gmail.com>
2022-11-17 15:17:48 +0330

    create new component for cards

---
+++ src/components/Cards.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Cards = () => {
+    return (
+        <section>
+            <div className='img-card-container'>
+                
+            </div>
+        </section>
+    );
+};
+
+export default Cards;
