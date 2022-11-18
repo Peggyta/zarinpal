105ec54945abb5763df0168359735ab1e5af17b6
Peggyta <peggyta9096@gmail.com>
2022-11-18 13:48:26 +0330

    write html structure of content part

--- src/components/Features.js
+++ src/components/Features.js
@@ -17,9 +17,47 @@ const Features = () => {
                 </p>
             </div>
             <div className='feature-list'>
-                <div>
-
+                <div className='feature first-feature'>
+                    <MdOutlineCheckCircleOutline />
+                    <h4>ضمانتِ پرداخت</h4>
                 </div>
+                <p>
+                درگاه پرداخت زرین‌پال،
+                تجربه‌ی پرداخت اینترنتی آسان،
+                سریع و امن را به مشتریان کسب
+                و کارهای آنلاین هدیه می‌دهد.
+                </p>
+            </div>
+            <div className='feature-list'>
+                <div className='feature second-feature'>
+                    <MdCallSplit />
+                    <h4>مسیردهی هوشمند</h4>
+                </div>
+                <p>
+                استانداردهای متفاوت زرین‌پال در بازه‌های زمانی مختلف،
+                باعث می‌شود کاربران به بهترین
+                درگاه پرداخت با بالاترین میزان تراکنش‌های موفق هدایت شوند.
+                </p>
+            </div>
+            <div className='feature-list'>
+                <div className='feature third-feature'>
+                    <MdHeadsetMic />
+                    <h4>پشتیبانی ۲۴/۷</h4>
+                </div>
+                <p>
+                هفت روزِ هفته، با پشتیبانی ۲۴ ساعته‌ی زرین‌پال،
+                 آماده‌ی پاسخ‌گویی و راهنمایی به کاربران هستیم.
+                </p>
+            </div>
+            <div className='feature-list'>
+                <div className='feature fourth-feature'>
+                    <MdHeadsetMic />
+                    <h4>امنیتِ پرداخت</h4>
+                </div>
+                <p>
+                درگاه امن زرین‌پال، ضمانتی برای امنیت
+                اطلاعات بانکی افراد، هنگام پرداخت‌‌های اینترنتی آن‌هاست.
+                </p>
             </div>
         </article>
     );
