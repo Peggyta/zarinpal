cd48f32cb33ed63a98a142a9a7aae2f5166177ac
Peggyta <peggyta9096@gmail.com>
2022-11-19 13:51:12 +0330

    correct the html structure of Feature component

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../scss/Features.css';
 //Icons
 import { MdCallSplit } from "react-icons/md";
 import { MdHeadsetMic } from "react-icons/md";
@@ -18,46 +19,48 @@ const Features = () => {
             </div>
             <div className='feature-list'>
                 <div className='feature first-feature'>
-                    <MdOutlineCheckCircleOutline />
-                    <h4>ضمانتِ پرداخت</h4>
+                    <div>
+                        <MdOutlineCheckCircleOutline />
+                        <h4>ضمانتِ پرداخت</h4>
+                    </div>
+                    <p>
+                    درگاه پرداخت زرین‌پال،
+                    تجربه‌ی پرداخت اینترنتی آسان،
+                    سریع و امن را به مشتریان کسب
+                    و کارهای آنلاین هدیه می‌دهد.
+                    </p>
                 </div>
-                <p>
-                درگاه پرداخت زرین‌پال،
-                تجربه‌ی پرداخت اینترنتی آسان،
-                سریع و امن را به مشتریان کسب
-                و کارهای آنلاین هدیه می‌دهد.
-                </p>
-            </div>
-            <div className='feature-list'>
                 <div className='feature second-feature'>
-                    <MdCallSplit />
-                    <h4>مسیردهی هوشمند</h4>
-                </div>
-                <p>
-                استانداردهای متفاوت زرین‌پال در بازه‌های زمانی مختلف،
-                باعث می‌شود کاربران به بهترین
-                درگاه پرداخت با بالاترین میزان تراکنش‌های موفق هدایت شوند.
-                </p>
-            </div>
-            <div className='feature-list'>
+                    <div>
+                        <MdCallSplit />
+                        <h4>مسیردهی هوشمند</h4>
+                    </div>
+                    <p>
+                    استانداردهای متفاوت زرین‌پال در بازه‌های زمانی مختلف،
+                    باعث می‌شود کاربران به بهترین
+                    درگاه پرداخت با بالاترین میزان تراکنش‌های موفق هدایت شوند.
+                    </p> 
+                </div>     
                 <div className='feature third-feature'>
-                    <MdHeadsetMic />
-                    <h4>پشتیبانی ۲۴/۷</h4>
-                </div>
-                <p>
-                هفت روزِ هفته، با پشتیبانی ۲۴ ساعته‌ی زرین‌پال،
-                 آماده‌ی پاسخ‌گویی و راهنمایی به کاربران هستیم.
-                </p>
-            </div>
-            <div className='feature-list'>
+                    <div>
+                        <MdHeadsetMic />
+                        <h4>پشتیبانی ۲۴/۷</h4>
+                    </div>
+                    <p>
+                    هفت روزِ هفته، با پشتیبانی ۲۴ ساعته‌ی زرین‌پال،
+                    آماده‌ی پاسخ‌گویی و راهنمایی به کاربران هستیم.
+                    </p>
+                </div>    
                 <div className='feature fourth-feature'>
-                    <MdHeadsetMic />
-                    <h4>امنیتِ پرداخت</h4>
+                    <div>
+                        <MdHeadsetMic />
+                        <h4>امنیتِ پرداخت</h4>
+                    </div> 
+                    <p>
+                    درگاه امن زرین‌پال، ضمانتی برای امنیت
+                    اطلاعات بانکی افراد، هنگام پرداخت‌‌های اینترنتی آن‌هاست.
+                    </p>
                 </div>
-                <p>
-                درگاه امن زرین‌پال، ضمانتی برای امنیت
-                اطلاعات بانکی افراد، هنگام پرداخت‌‌های اینترنتی آن‌هاست.
-                </p>
             </div>
         </article>
     );
