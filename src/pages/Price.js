e5daa5ea4006afb89288bfc3b54fa8f7a855ad34
Peggyta <peggyta9096@gmail.com>
2022-11-21 15:11:16 +0330

    want to create a price table using css grid

--- src/pages/Price.js
+++ src/pages/Price.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import '../pagesStyle/Price.css';
+//Components
+import StartBox from '../components/StartBox';
 //Image & Icons
 import PricePic from '../images/pricepic.jpg';
 import { FiChevronDown } from 'react-icons/fi';
 
+
 const Price = () => {
     return (
+        <>
         <div className='price-container'>
            <div className='price-description'>
                 <div className='price-image'>
                     <img src={PricePic} alt='image' />
                 </div>
-                <div className='price-description'>
+                <div className='price-text-content'>
                     <h3>تعرفه‌ها</h3>
                     <p>زرین‌‌پال با ارائه‌‌ی یک فرمول شفاف و منصفانه برای کارمزدها
                     ، امکانی فراهم کرده تا کاربران جدید، در ماه اول عضویت، تسویه‌‌حساب خود را به
@@ -24,7 +29,28 @@ const Price = () => {
                 <div></div>
                 <FiChevronDown className='down-icon' />
            </div>
+           <div className='price-list'>
+            <h3>تعرفه‌‌ها در زرین‌‌پال</h3>
+                <div className='price-grid-table'>
+                    <div className='grid-item'>تسویه‌‌حساب در ماه اول‌</div>
+                    <div className='grid-item'>رایگان</div>
+                    <div className='grid-item'>تسویه‌حساب به حساب بانکی</div>
+                    <div className='grid-item'>۱٪ تا سقف ۳۰۰۰تومان به ازای هر تراکنش</div>
+                    <div className='grid-item'>زرین‌‌لینک</div>
+                    <div className='grid-item'>۲۰۰تومان به ازای هر تراکنش از پرداخت‌‌کننده</div>
+                    <div className='grid-item'>تسویه‌‌حساب زرین‌‌کارت</div>
+                    <div className='grid-item'>۰.۵٪ تا سقف ۲۰۰۰ تومان به ازای هر تراکنش</div>
+                    <div className='grid-item'>تسویه‌حساب در سه‌شنبه‌های طلایی</div>
+                    <div className='grid-item'>رایگان</div>
+                </div>
+                <div className='contact-us-container'>
+                    <h3></h3>
+                </div>
+           </div>
+           <StartBox />
         </div>
+        </>
+        
     );
 };
 
